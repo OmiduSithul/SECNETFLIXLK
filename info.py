68feac3105c471652436b3945fa4044b858b7c34
Omidu Sithul <97288848+OmiduSithul@users.noreply.github.com>
2022-11-20 15:17:23 +0530

    databse changed

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = environ.get('AUTH_CHANNEL', '-1001648999264')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sahan:sahan@cluster0.mbmvu.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sahan:sahan@cluster0.aypsk.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "sahan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
